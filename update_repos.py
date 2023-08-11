5a5cf0f5bc6a3d59f84d48da725603cebacb00a2
Jean-Marie GERONIMI <geronimi@goodbarber.com>
2023-08-11 10:17:24 +0200

    correct indent for py files

--- update_repos.py
+++ update_repos.py
@@ -6,32 +6,33 @@ import json
 
 from pygithub3 import Github
 
-repos_out = 'repos.json'
+repos_out = "repos.json"
 
 auth = netrc.netrc()
 
 try:
-  (user, _, token) = auth.authenticators('api.github.com')
-  ghclient = Github(token)
-  logged_in = True
-  print("Logged in")
+    (user, _, token) = auth.authenticators("api.github.com")
+    ghclient = Github(token)
+    logged_in = True
+    print("Logged in")
 except Exception as e:
-  ghclient = Github()
-  logged_in = False
-  print(f"Not logged in with error : {e}")
-  
+    ghclient = Github()
+    logged_in = False
+    print(f"Not logged in with error : {e}")
+
+
 def gh_repos():
-  print('Fetching all repos information...')
+    print("Fetching all repos informations...")
 
-  if not logged_in:
-    print('Not logged in.  Please login to GitHub.')
-    time.sleep(2.0) # Take a nap so GitHub doesn't aggressively throttle us.
+    if not logged_in:
+        print("Not logged in.  Please login to GitHub.")
+        time.sleep(2.0)  # Take a nap so GitHub doesn't aggressively throttle us.
 
-  repos = ghclient.get_user().get_repos()
-  repos_formatted = {repo.name: repo.topics for repo in repos}
+    repos = ghclient.get_user().get_repos()
+    repos_formatted = {repo.name: repo.topics for repo in repos}
 
-  with open(repos_out, 'w') as f:
-      json.dump(repos_formatted, f, indent=4) 
+    with open(repos_out, "w") as f:
+        json.dump(repos_formatted, f, indent=4)
 
 
 gh_repos()
